11dde5b489629936f9c16acf8c744c649fdc405a
James Tang <james@goodnotesapp.com>
2021-02-09 13:41:07 +0800

    Initial Commit

---
+++ testLoadImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testLoadImage
+//
+//  Created by James Tang on 9/2/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
