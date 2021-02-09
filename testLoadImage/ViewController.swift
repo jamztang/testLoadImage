3bc7aad0d05235a28582b801daa3d8f894ad73bf
James Tang <james@goodnotesapp.com>
2021-02-09 13:47:48 +0800

    Loading jpg2k image in iOS 14.2 success

--- testLoadImage/ViewController.swift
+++ testLoadImage/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var loadButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+
     }
 
+    @IBAction func loadButtonDidPress(_ sender: Any) {
 
+        guard let path = Bundle.main.path(forResource: "jpg2k", ofType: "") else {
+            fatalError("no resources")
+        }
+        let image = UIImage(contentsOfFile: path)
+
+        imageView.image = image
+    }
+
+    @IBAction func clearButtonDidPress(_ sender: Any) {
+        imageView.image = nil
+    }
 }
 
